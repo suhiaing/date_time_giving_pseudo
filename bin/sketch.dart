6031e54a88b240d658bd990229fabd1d3026fb8a
suhiaing <suhlaingthin288@gmail.com>
2024-04-27 17:20:05 +0630

    only date_and_time fun added in class

---
+++ bin/sketch.dart
@@ -0,0 +1,5 @@
+import 'package:sketch/sketch.dart' as sketch;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sketch.calculate()}!');
+}
---
+++ test/sketch_test.dart
@@ -0,0 +1,8 @@
+import 'package:sketch/sketch.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
